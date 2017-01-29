4bc6cb3b60cb0a51b77270b064d19796d889afb7
Mailo Svetel <development@rooland.cz>
2017-01-29 20:47:12 +0100

    [model] More validation

--- app/models/watching.rb
+++ app/models/watching.rb
@@ -1,3 +1,6 @@
 class Watching < ApplicationRecord
   has_and_belongs_to_many :yusers
+
+  validates_presence_of :url
+  validates_format_of   :url, with: /\Ahttp/i
 end
--- app/models/yuser.rb
+++ app/models/yuser.rb
@@ -1,3 +1,5 @@
 class Yuser < ApplicationRecord
   has_and_belongs_to_many :watchings
+
+  validates_presence_of :username
 end
